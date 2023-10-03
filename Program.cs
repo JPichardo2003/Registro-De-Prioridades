075e73a4491ce40e8bcf6b2824fc090062b36532
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-10-03 12:50:30 -0400

    Quitando datos del wheaterforecats en el program

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
-using RegistroDePrioridades.Data;
 using RegistroDePrioridades.DAL;
 using RegistroDePrioridades.BLL;
 
@@ -12,18 +11,11 @@ namespace RegistroDePrioridades
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            //leer la ConnectionString llamada "ConStr" que pusimos en appsettings.json
             var ConStr = builder.Configuration.GetConnectionString("ConStr");
-            //Inyectar el contexto para que este disponible en los construtores
-            //donde lo solicitemos
             builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConStr));
-            //Inyectar todas las bll para poder recibirlas en cualquier registro
             builder.Services.AddScoped<PrioridadesBLL>();
-
-            // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
-            builder.Services.AddSingleton<WeatherForecastService>();
 
             var app = builder.Build();
 
